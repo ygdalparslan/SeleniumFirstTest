7087fb6074296f32106ab44b25598ca90319aa4a
ygdalparslan <ygdalparslan@gmail.com>
2023-09-02 23:49:47 +0300

    First Test

---
+++ src/test/java/selenium01/FirstTest.java
@@ -0,0 +1,20 @@
+package selenium01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.trendyol.com");
+
+
+    }
+
+
+
+}
